23771aa78f771f56e0942081446e6ba2058e1ac7
Rahim Rahimli <ragim95@gmail.com>
2020-03-28 12:12:06 +0400

    introduce MergeSort

--- src/main/java/com/bakudynamics/sort/InsertionSort.java
+++ src/main/java/com/bakudynamics/sort/InsertionSort.java
@@ -1,7 +1,5 @@
 package com.bakudynamics.sort;
 
-import com.bakudynamics.utils.Logger;
-
 public class InsertionSort implements Sort {
 
     @Override
@@ -11,22 +9,16 @@ public class InsertionSort implements Sort {
 
     @Override
     public void sort(int[] a, Order order) {
+        if (a == null || a.length == 0) return;
         for (int i = 1; i < a.length; i++) {
             int key = a[i];
             int j = i - 1;
 
-            Logger.debug(a);
-            Logger.debug("i=%d, a[i] = key = %d; j = %d\n", i, key, j);
-            Logger.debug("swapping...");
             while (j >= 0 && direction(key, a[j], order)) {
-                Logger.debug("j = %d; a[j]=%d, a[j+1]=%d\n", j, a[j], a[j + 1]);
                 a[j + 1] = a[j];
                 j--;
             }
             a[j + 1] = key;
-            Logger.debug("swap done");
-            Logger.debug(a);
-            Logger.debug("");
         }
     }
 
--- src/main/java/com/bakudynamics/sort/SortTest.java
+++ src/main/java/com/bakudynamics/sort/SortTest.java
@@ -25,6 +25,10 @@ public class SortTest {
     public static Collection<Object[]> data() {
         return List.of(new Object[][]{
                 {new int[]{3, 2, 1}, new int[]{1, 2, 3}, Sort.Order.ASC},
+                {new int[]{0}, new int[]{0}, Sort.Order.ASC},
+                {new int[]{}, new int[]{}, Sort.Order.ASC},
+                {new int[]{-1, -2, -3, 0, 3, 2, 1}, new int[]{-3, -2, -1, 0, 1, 2, 3}, Sort.Order.ASC},
+                {null, null, Sort.Order.ASC},
                 {new int[]{5, 2, 4, 6, 1, 3}, new int[]{1, 2, 3, 4, 5, 6}, Sort.Order.ASC},
                 {new int[]{5, 2, 4, 6, 1, 3}, new int[]{6, 5, 4, 3, 2, 1}, Sort.Order.DESC}
         });
